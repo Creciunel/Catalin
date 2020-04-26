378556c277eedab55d8ecf9b58eb182dfb3a5c3e
Catalin <cr.catalin@mail.ru>
2020-04-26 12:36:50 +0300

    First program in c, print Hello word

---
+++ language-c/print-hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h> //conect standart input output library
+
+int main() //main function
+{
+    printf("Hello word"); //print function and text print
+    return 0;             //without error
+}
